2682c07d0acbf309a7687a5c0099353d3614be69
olea2019 <obiriucova@gmail.com>
2019-06-22 16:52:31 +0300

    remove unused constants

--- sources/constants/argumentTypes.ts
+++ sources/constants/argumentTypes.ts
@@ -7,15 +7,7 @@ export interface ArgumentType {
 }
 
 export const ARGUMENT_TYPES: ArgumentType[] = [
-  { shortUnix: Argument.ASCII, longUnix: '--ascii', windows: '/A' },
-  { shortUnix: Argument.BINARY, longUnix: '--binary', windows: '/B' },
   { shortUnix: Argument.DELETE, longUnix: '--delete', windows: '/D' },
-  { shortUnix: Argument.ENCRYPT, longUnix: '--encrypt', windows: '/E' },
   { shortUnix: Argument.HELP, longUnix: '--help', windows: '/H' },
-  { shortUnix: Argument.INTERACTIVE, longUnix: '--interactive', windows: '/I' },
-  { shortUnix: Argument.LINK, longUnix: '--link', windows: '/L' },
-  { shortUnix: Argument.NETWORK_FILES, longUnix: '--network', windows: '/Z' },
   { shortUnix: Argument.OVERWRITE, longUnix: '--overwrite', windows: '/O' },
-  { shortUnix: Argument.SHORT_NAME, longUnix: '--short-name', windows: '/N' },
-  { shortUnix: Argument.VERIFY, longUnix: '--verify', windows: '/V' },
 ];
--- sources/interfaces/arguments.ts
+++ sources/interfaces/arguments.ts
@@ -1,13 +1,5 @@
 export enum Argument {
-  ASCII = '-a',
-  BINARY = '-b',
-  ENCRYPT = '-e',
   DELETE = '-d',
-  VERIFY = '-v',
-  SHORT_NAME = '-n',
   OVERWRITE = '-o',
-  NETWORK_FILES = '-z',
-  LINK = '-l',
   HELP = '-h',
-  INTERACTIVE = '-i',
 }
